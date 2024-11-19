0ca3ea83f02ef528f81035b06051130617e95e7a
thaianh4780 <86099409+TruongThaiAnh@users.noreply.github.com>
2024-11-19 11:09:39 +0700

    add weapon collision + note progress

--- prj-RPG/Assets/Scripts/Sword.cs
+++ prj-RPG/Assets/Scripts/Sword.cs
@@ -6,7 +6,9 @@ public class Sword : MonoBehaviour
 {
     [SerializeField] private GameObject slashAnimPrefab;
     [SerializeField] private Transform slashAnimSpawnPoint;
-    
+    [SerializeField] private Transform weaponCollider;
+
+
     private PlayerControls playerControls;
     private Animator myAnimator;
     private PlayerController playerController;
@@ -42,12 +44,18 @@ public class Sword : MonoBehaviour
     {
         myAnimator.SetTrigger("Attack");
 
+        weaponCollider.gameObject.SetActive(true);
+
         slashAnim = Instantiate(slashAnimPrefab,slashAnimSpawnPoint.position,Quaternion.identity);
         slashAnim.transform.parent = this.transform.parent;
     }
 
+    private void DoneAttackingAnimeEvent()
+    {
+        weaponCollider.gameObject.SetActive(false);
+    }
 
-    public void SwingUpFlipAnim()
+    public void SwingUpFlipAnimEvent()
     {
         slashAnim.gameObject.transform.rotation = Quaternion.Euler(-180,0,0);
 
@@ -57,7 +65,7 @@ public class Sword : MonoBehaviour
         }
     }
 
-    public void SwingDownFlipAnim()
+    public void SwingDownFlipAnimEvent()
     {
         slashAnim.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
 
@@ -78,10 +86,13 @@ public class Sword : MonoBehaviour
         if (mousePos.x < playerScreenPoint.x)
         {
             activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
         }
         else
         {
             activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle); // Nếu chuột ở bên phải, kiếm giữ nguyên trục X
+            weaponCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
+
         }
     }
 
