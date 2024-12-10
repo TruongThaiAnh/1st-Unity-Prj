d2542d2e761629ddc6c5b432a4a48f12fbed0a5e
thaianh4780 <86099409+TruongThaiAnh@users.noreply.github.com>
2024-12-10 12:07:10 +0700

    add the fade transition when move to map

--- prj-RPG/Assets/Scripts/AreaExit.cs
+++ prj-RPG/Assets/Scripts/AreaExit.cs
@@ -9,13 +9,26 @@ public class AreaExit : MonoBehaviour
 
     [SerializeField] private string senceTransitionName;
 
+    private float waitToLoadTime = 1f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>())
         {
-            SceneManager.LoadScene(sceneToLoad);
             SceneManagement.Instance.SetTransitionName(senceTransitionName);
+            UIFade.Instance.FadeToBlack();
+            StartCoroutine(LoadSceneRoutine());
         };
+    }
+
+    private IEnumerator LoadSceneRoutine()
+    {
+        while (waitToLoadTime >= 0) {
+            waitToLoadTime -= Time.deltaTime;  
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneToLoad);
     }
 
 
